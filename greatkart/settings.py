8922c4eaf4148af23404a62b1b8b0932d1e1c75d
Your Name <you@example.com>
2021-09-17 21:32:11 +0530

    deploying to s3

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['greatkart-tutorial-env.eba-qps9nqic.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-tutorial-env.eba-qps9nqic.us-west-2.elasticbeanstalk.com', '*']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +45,8 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
-    'admin_honeypot'
+    'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +114,20 @@ else:
     }
 
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ebdb',
+#         'USER': 'greatkartuser',
+#         'PASSWORD': '9980abcd',
+#         'HOST': 'aahz1cfvfnoxsu.cxpojmsgc53g.us-west-2.rds.amazonaws.com',
+#         'PORT': 5432,
+#     }
+# }
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -149,14 +165,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#       'greatkart/static',
+# ]
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
-      'greatkart/static',
+    'greatkart/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
